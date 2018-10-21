0969bf0e6c6771a63f2a7c10542032175d2605c5
ampeo <44274446+ampeo@users.noreply.github.com>
2018-10-21 23:15:54 +0300

    Add files via upload

--- class/class/class.cpp
+++ class/class/class.cpp
@@ -5,53 +5,43 @@
 #include <iostream>
 
 using namespace std;
-
-class math {
-private:
-	double c;
+class math {	
 public:
+	const float pi = 3.1415;
+	const float e = 2.7182;
 	double sum (double a, double b){
-		c = a + b;
-		cout<<a<<"+"<<b<<"="<<c<<endl;
-		return (c);
+		a += b;
+		return (a);
 	}
 	double sub(double a, double b) {
-		c = a - b;
-		cout << a << "-" << b << "=" << c<<endl;
-		return(c);
+		a = a - b;
+		return(a);
 	}
 	double mul(double a, double b) {
-		c = a * b;
-		cout << a << "*" << b << "=" << c << endl;
-		return(c);
+		a = a * b;
+		return(a);
 	}
 	double div(double a, double b) {
-		c = a / b;
-		cout << a << "/" << b << "=" << c << endl;
-		return(c);
+		a = a / b;
+		return(a);
 	}
 	double abs(double a) {
 		if (a < 0) a = a*(-1);
-		cout << a<<endl;
 		return(a);
 	}
 	double max(double a, double b) {
 		if (a > b) {
-			cout <<"max="<< a<<endl;
 			return(a);
 		}
 		else {
-			cout << "max=" << b<<endl;
 			return(b);
 		}
 	}
 	double min(double a, double b) {
 		if (a < b) {
-			cout << "min=" << a << endl;
 			return(a);
 		}
 		else {
-			cout << "min=" << b;
 			return(b);
 		}
 	}
@@ -62,7 +52,6 @@ public:
 			b *= i;
 			i++;
 		}
-		cout << "Facterial:" << b << endl;
 		return(b);
 	}
 	int round(double a) {
@@ -75,50 +64,33 @@ public:
 		}
 		else
 			b /= 10;
-		cout <<"Okryglenie"<< b << endl;
 		return(b);
 	}
 	int pow(int a, int b) {
 		for (; b != 1; b--) {
 			a *= a;
 		}
-		cout << "stepen'=" << a<<endl;
 		return(a);
 	}
 	double hypot(double a, double b) {
 		a = sqrt(a*a + b*b);
-		cout << "gipotenyza=" << a<<endl;
 		return(a);
 	}
 	double RadToDegr(double a) {
 		a = a * 57.3;
-		cout << "Iz Rad B Gradyci:"<<a<<endl;
 		return(a);
 	}
 	double DegrToRad(double a) {
 		a = a*0.017;
-		cout << "Iz Rad B Gradyci:"<<a<<endl;
 		return(a);
 	}
 };
 int main()
 {
 	double a, b;
-	math c;
-	cin >> a >> b;
-	c.sum (a, b);
-	c.sub(a, b);
-	c.mul(a, b);
-	c.div(a, b);
-	c.abs(a);
-	c.max(a, b);
-	c.min(a, b);
-	c.fact(a);
-	c.round(a);
-	c.pow(a, b);
-	c.hypot(a, b);
-	c.RadToDegr(a);
-	c.DegrToRad(a);
+	math c,c1;
+	a = c.sum(c1.sum(1, 2), 3);
+		cout << a;
 	system("pause");
     return 0;
 }
